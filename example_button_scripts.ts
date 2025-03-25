1e162f61551b5aee256c3ecee05d45808bf61e3a
dakongwuxian <33227493+dakongwuxian@users.noreply.github.com>
2025-03-25 17:08:02 +0800

    Add files via upload

---
+++ example_button_scripts.ts
@@ -0,0 +1,57 @@
+<tab {test page 1}>
+<button {0} {multi-lines command 1}>
+<data>
+line 1
+wait 1
+line 2
+wait 1
+line 3
+wait 1
+</data>
+<icon>
+
+</icon>
+<desc>
+
+</desc>
+</button>
+<button {1} {multi-lines command 2}>
+<data>
+line 11
+wait for # for 3
+wait 1
+line 22
+wait for # for 3
+wait 1
+line 33
+wait for # for 3
+wait 1
+</data>
+<icon>
+
+</icon>
+<desc>
+
+</desc>
+</button>
+</tab>
+
+<tab {test page 2}>
+<button {0} {multi-lines command 3}>
+<data>
+line 111
+wait 1
+line 222
+wait 1
+line 333
+wait 1
+</data>
+<icon>
+
+</icon>
+<desc>
+
+</desc>
+</button>
+</tab>
+
